fe40c057d4ad0bfc76d166c6191402628be97087
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-13 15:08:42 +1000

    Added a testMode option so we can test things.

--- js/wpajax.js
+++ js/wpajax.js
@@ -58,7 +58,8 @@
             cacheRequests: true,  // Use HTML5 localStorage to cache pages?
             cacheExpiry: 1,          // Time in hours cached items should be cached for
             waitForImages: true,   // Wait for images to load before loading a page?
-            debug: false               // Debug mode shows console messages if true,
+            debug: false,              // Debug mode shows console messages if true
+            testMode: false           // Modifies certain methods so we can test them
         },
         o = settings;
 
