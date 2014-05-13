602dd36e6d96d324236184e210afb52f7b20ee73
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-13 14:39:24 +1000

    More tests (configuration and request status).

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,18 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+// Check calling the configure method without an option
+test("Configure Method (Without Options)", function() {
+    var result = wpAjax.configure();
+    equal(typeof(result), "object");
+});
+
+// Test setting an option and checking the value was saved
+test("Configure Method (With Option)", function() {
+    var instance = wpAjax.configure({debug:true});
+    var result      = instance.getConfiguration();
+    equal(result.debug, true);
+});
+
+// Test getting the request status
+test("Request Status", function() {
+    var result = wpAjax.requestStatus();
+    equal(result, false);
 });
