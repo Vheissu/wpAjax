f53b7030c0a9fe15cd469fe27777ddf07ed4cdaa
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-13 14:47:54 +1000

    Deep equal for comparison on types. Added in a test for the current URL method.

--- tests/tests.js
+++ tests/tests.js
@@ -1,18 +1,24 @@
 // Check calling the configure method without an option
 test("Configure Method (Without Options)", function() {
     var result = wpAjax.configure();
-    equal(typeof(result), "object");
+    deepEqual(typeof(result), "object");
 });
 
 // Test setting an option and checking the value was saved
 test("Configure Method (With Option)", function() {
     var instance = wpAjax.configure({debug:true});
     var result      = instance.getConfiguration();
-    equal(result.debug, true);
+    deepEqual(result.debug, true);
 });
 
 // Test getting the request status
 test("Request Status", function() {
     var result = wpAjax.requestStatus();
-    equal(result, false);
+    deepEqual(result, false);
+});
+
+test("Get Current URL", function() {
+    var result      = wpAjax.getCurrentUrl();
+    var expected = window.location.href;
+    equal(result, expected);
 });
