e63a3fde8d43876612512b4f3ed4cb036163db41
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-13 15:09:22 +1000

    jQuery AJAX Fetch Page Title test

--- tests/tests.js
+++ tests/tests.js
@@ -6,9 +6,10 @@ test("Configure Method (Without Options)", function() {
 
 // Test setting an option and checking the value was saved
 test("Configure Method (With Option)", function() {
-    var instance = wpAjax.configure({debug:true});
-    var result      = instance.getConfiguration();
+    wpAjax.configure({debug:true, testMode:true});
+    var result      = wpAjax.getConfiguration();
     ok(result.debug);
+    wpAjax.configure({debug:false});
 });
 
 // Test getting the request status
@@ -20,5 +21,11 @@ test("Request Status", function() {
 test("Get Current URL", function() {
     var result      = wpAjax.getCurrentUrl();
     var expected = window.location.href;
+    equal(result, expected);
+});
+
+test("Fetch Page Title", function() {
+    var result      = wpAjax.getTitle("ajax-test.html");
+    var expected = "Valid Response";
     equal(result, expected);
 });
