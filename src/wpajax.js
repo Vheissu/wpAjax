52b0d820e49d74cacc3815f7a64a4ac0a31fd4d2
Dwayne Charrington <dwayne@josephmark.com.au>
2014-06-18 20:48:49 +1000

    Console log for debugging.

--- src/wpajax.js
+++ src/wpajax.js
@@ -268,6 +268,9 @@
         var _content = _html.find(o.content).html();
         var _wpvars = _html.find("#wpvars").html();
 
+        console.log("Process reqquest");
+        console.log(_wpvars);
+
         log("processRequest: Processing the data returned by a successful AJAX call");
 
         // Do we have content?
@@ -280,7 +283,7 @@
             s.id = "wpvars";
             s.type = "text/javascript";
             s.text = _wpvars;
-            document.body.appendChild(s);
+            $body.append(s);
 
             var completeEvent = jQuery.Event("wpAjax.complete", {
                 previousUrl: previousUrl,
