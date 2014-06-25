6e9236f80de0455ce3de40c68ee1278bd08d0b34
Dwayne Charrington <dwayne@josephmark.com.au>
2014-06-25 13:43:01 +1000

    Added in Gulp plumber for preventing errors breaking things when compiling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp     = require('gulp'),
-     plugins = require('gulp-load-plugins')({camelize: true});
+     plugins = require('gulp-load-plugins')({camelize: true}),
+     plumber = require('gulp-plumber');
+
+var onError = function (err) {
+  console.log(err);
+};
 
 // Lets us type "gulp" on the command line and run all of our tasks
 gulp.task('default', ['jshint', 'vendor', 'scripts']);
@@ -7,6 +12,7 @@ gulp.task('default', ['jshint', 'vendor', 'scripts']);
 // Hint all user-developed JS
 gulp.task('jshint', function() {
   return gulp.src(['src/*.js'])
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('default'))
     .pipe(plugins.notify({ message: 'JS Hinting task complete' }));
@@ -15,6 +21,7 @@ gulp.task('jshint', function() {
 // Combine, minify all vendor JS
 gulp.task('vendor', function() {
   return gulp.src('src/vendor/*.js')
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(plugins.concat('wpajax-vendor.min.js'))
     .pipe(plugins.uglify())
     .pipe(gulp.dest('js/'))
@@ -24,6 +31,7 @@ gulp.task('vendor', function() {
 // JS concat, strip debugging and minify
 gulp.task('scripts', function() {
   return gulp.src(['src/*.js', '!src/vendor{,/**}'])
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(plugins.concat('wpajax.min.js'))
     //.pipe(plugins.stripDebug())
     .pipe(plugins.uglify())
